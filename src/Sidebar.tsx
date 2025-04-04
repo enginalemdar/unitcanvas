3944f8b67831033aaee3c5e8d58f848f5cb31162
enginalemdar <162575473+enginalemdar@users.noreply.github.com>
2025-04-04 20:13:13 +0300

    Create Sidebar.tsx

---
+++ src/Sidebar.tsx
@@ -0,0 +1,16 @@
+import DragItem from './DragItem';
+import { ItemTypes } from './dnd/types';
+import { Circle, Square, Text, ArrowRight } from 'lucide-react';
+
+const Sidebar = () => {
+  return (
+    <div className="w-20 bg-gray-100 p-2 flex flex-col gap-4 items-center shadow-md">
+      <DragItem type={ItemTypes.RECT} icon={<Square size={24} />} />
+      <DragItem type={ItemTypes.CIRCLE} icon={<Circle size={24} />} />
+      <DragItem type={ItemTypes.TEXT} icon={<Text size={24} />} />
+      <DragItem type={ItemTypes.ARROW} icon={<ArrowRight size={24} />} />
+    </div>
+  );
+};
+
+export default Sidebar;
