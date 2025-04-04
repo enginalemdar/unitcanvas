62462f208de4aa56a06a9f76fc4830743e8a1f3a
enginalemdar <162575473+enginalemdar@users.noreply.github.com>
2025-04-04 19:53:45 +0300

    Update Controls.tsx

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -32,37 +32,3 @@ const Controls = ({ onSave, onLoad, onClear }: Props) => {
 };
 
 export default Controls;
-import React from 'react';
-
-type Props = {
-  onSave: () => void;
-  onLoad: () => void;
-  onClear: () => void;
-};
-
-const Controls = ({ onSave, onLoad, onClear }: Props) => {
-  return (
-    <div className="flex justify-center gap-4 mb-4">
-      <button
-        onClick={onSave}
-        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-      >
-        Kaydet
-      </button>
-      <button
-        onClick={onLoad}
-        className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
-      >
-        Yükle
-      </button>
-      <button
-        onClick={onClear}
-        className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
-      >
-        Temizle
-      </button>
-    </div>
-  );
-};
-
-export default Controls;
